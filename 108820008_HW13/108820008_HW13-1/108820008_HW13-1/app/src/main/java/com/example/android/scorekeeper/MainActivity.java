377491f467a7a6e720539c87503b9e4a41e68c70
Ricky Hu <ricky9667@gmail.com>
2022-05-03 20:13:08 +0800

    Finish Homework 13-1

---
+++ 108820008_HW13/108820008_HW13-1/108820008_HW13-1/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.scorekeeper;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+/***
+ * Main Activity for the Scorekeeper app, which keeps score for any game
+ * involving two teams. The user can increment or decrement the score
+ * for each team using Button views.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
+    private String preferencesFile = "com.example.android.scorekeeper";
+
+    // Member variables for holding the score
+    private int mScore1;
+    private int mScore2;
+
+    // Member variables for the two score TextView elements
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    // Tags to be used as the keys in OnSavedInstanceState
+    static final String STATE_SCORE_1 = "score1";
+    static final String STATE_SCORE_2 = "score2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(preferencesFile, MODE_PRIVATE);
+
+        //Find the TextViews by ID
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+        mScore1 = preferences.getInt(STATE_SCORE_1, 0);
+        mScore2 = preferences.getInt(STATE_SCORE_2, 0);
+
+        mScoreText1.setText(String.valueOf(mScore1));
+        mScoreText2.setText(String.valueOf(mScore2));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor preferencesEditor = preferences.edit();
+        preferencesEditor.putInt(STATE_SCORE_1, mScore1);
+        preferencesEditor.putInt(STATE_SCORE_2, mScore2);
+        preferencesEditor.apply();
+    }
+
+    /**
+     * Handles the onClick of both the decrement buttons.
+     *
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+        // Get the ID of the button that was clicked.
+        int viewID = view.getId();
+        switch (viewID) {
+            // If it was on Team 1:
+            case R.id.decreaseTeam1:
+                // Decrement the score and update the TextView.
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            // If it was Team 2:
+            case R.id.decreaseTeam2:
+                // Decrement the score and update the TextView.
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    /**
+     * Handles the onClick of both the increment buttons.
+     *
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        // Get the ID of the button that was clicked.
+        int viewID = view.getId();
+        switch (viewID) {
+            // If it was on Team 1:
+            case R.id.increaseTeam1:
+                // Increment the score and update the TextView.
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            // If it was Team 2:
+            case R.id.increaseTeam2:
+                // Increment the score and update the TextView.
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    /**
+     * Creates the night mode menu option.
+     *
+     * @param menu The menu in the action bar
+     * @return True to display the menu, false to hide it
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+
+    /**
+     * Handles options menu item clicks.
+     *
+     * @param item The item that was pressed
+     * @return returns true since the item click wa handled
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check if the correct item was clicked.
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            // Set the theme mode for the restarted activity.
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate();
+        }
+        return true;
+    }
+}
